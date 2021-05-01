ce99b45691ed01f01b07241cab2352ff8018bd95
Veronika Dmytryk <veronika.dmytryk@gmail.com>
2021-05-01 02:11:20 -0700

    made lambda call work

--- backend/scheduleDBupdate/app/lambda_function.py
+++ backend/scheduleDBupdate/app/lambda_function.py
@@ -37,7 +37,8 @@ def lambda_handler(event, context):
 	for document in response_body:
 		if 'lastUpdated' in document.keys() and (todaysDate - document["lastUpdated"]).days >= 7:
 			# send an asyncronous processing request
-			sendProcessingRequest(document["hotelId"], document["lastUpdated"])
+			print(document)
+			sendProcessingRequest(document["propertyId"], dateToString(document["lastUpdated"]))
 
 	# Version 2 - Put to Queue
 #	for document in response_body:
@@ -60,18 +61,21 @@ def lambda_handler(event, context):
 
 def sendProcessingRequest(hotelId, lastUpdated):
         payload = {}
-        payload['hotelId'] = hotelId
+        payload['propertyId'] = hotelId
         payload['lastUpdated'] = lastUpdated
-        print(payload)
         lam.invoke(FunctionName='updateDB',
                InvocationType='Event',
                Payload=json.dumps(payload))
         
-#def main():
-#	print("hello from local machine")
-#	lambda_handler({"httpMethod":"GET", "queryStringParameters" : {
-#		"token": "yJraWQiOiJ5OTMwTWxtbGRndmY4ZXFZQ2EzWGgrdXpUbE9qMFQ3ZFV2UUV1SDJJbk9FPSIsImFsZyI6IlJTMjU2In0.eyJhdF9oYXNoIjoic3R1YnBKejQ1aUItWlZGeFZ6UUh6dyIsInN1YiI6IjdjMDkwNDJiLTJiNDUtNGNjYy1iMTdjLTMyNzEyNGJmZjE2NiIsImNvZ25pdG86Z3JvdXBzIjpbInVzLWVhc3QtMV9pckhXUmxYN2JfR29vZ2xlIl0sImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLnVzLWVhc3QtMS5hbWF6b25hd3MuY29tXC91cy1lYXN0LTFfaXJIV1JsWDdiIiwiY29nbml0bzp1c2VybmFtZSI6Imdvb2dsZV8xMTA5NTc5Mzc4OTIyOTE1ODMzNzkiLCJub25jZSI6IkpUTXdMeU1EUm51LVRZVW1PVlVueEV4cUEtZWt2XzZKdzRvWHRHdFJNZVlWaWROUGE2YkxGU2xtNGJ0eXJQYnVxY0xlZ21CTzZlYXFtemtqRFMtUlFiOHlRTzc2Wi1TSFdTV2x3YTJuTHduMGs5bHRoaVBhT0thNUU1QUJETFFLdUJFeHZhZkZ6QzFLaGwwTzRnbnI0RmdfNlVORFpWd2E4dWU5ajZZMlcxTSIsImF1ZCI6IjZ0ZWk0YnAzbnVwcHA1bXJlbmIwczQ5NmJxIiwiaWRlbnRpdGllcyI6W3sidXNlcklkIjoiMTEwOTU3OTM3ODkyMjkxNTgzMzc5IiwicHJvdmlkZXJOYW1lIjoiR29vZ2xlIiwicHJvdmlkZXJUeXBlIjoiR29vZ2xlIiwiaXNzdWVyIjpudWxsLCJwcmltYXJ5IjoidHJ1ZSIsImRhdGVDcmVhdGVkIjoiMTYxMzIwOTg1OTIwOCJ9XSwidG9rZW5fdXNlIjoiaWQiLCJhdXRoX3RpbWUiOjE2MTUyMDY4M#TksIm5hbWUiOiJWZXJvbmlrYSBEbXl0cnlrIiwiZXhwIjoxNjE1MjEwNDE5LCJpYXQiOjE2MTUyMDY4MTksImVtYWlsIjoidmVyb25pa2EuZG15dHJ5a0BnbWFpbC5jb20ifQ.PvRLka4FHmJUQXy7NrP5siJqdkJ3rEUYMvtSU9QayyIef0GOCMxH_VP359LPl8wmvp1Z5wYRpt6u5nt4ygKELByeiEWLB2y07wFH6MvtHHaIWF2GPpM6QCP5KGZt2gQJgKpmC88D0ZyzasQKYADLIUj7vlX4HOPMnAMNJ3LLnGohHFNVRwJJLeElBp8t4eiQiENOdoFsNd30Y_Rff6PXoyCONY_sg0_-SwG-MdOXIto-VxoMkUGQqrbu3bdkCafckvA_QqiQgFt_xkRoqLOFkAaximA3PTEzA6CsLvq58d4WqctTi-hFZOl3eMlY8oBbeLSc2J0zWNDv_pUO8h_XHg",
-#		"property_id": "111" }}, None)
+def dateToString(o):
+    if isinstance(o, datetime):
+        return o.__str__()
 
-#if __name__ == "__main__":
-#	main()
+def main():
+	print("hello from local machine")
+	lambda_handler({"httpMethod":"GET", "queryStringParameters" : {
+		"token": "yJraWQiOiJ5OTMwTWxtbGRndmY4ZXFZQ2EzWGgrdXpUbE9qMFQ3ZFV2UUV1SDJJbk9FPSIsImFsZyI6IlJTMjU2In0.eyJhdF9oYXNoIjoic3R1YnBKejQ1aUItWlZGeFZ6UUh6dyIsInN1YiI6IjdjMDkwNDJiLTJiNDUtNGNjYy1iMTdjLTMyNzEyNGJmZjE2NiIsImNvZ25pdG86Z3JvdXBzIjpbInVzLWVhc3QtMV9pckhXUmxYN2JfR29vZ2xlIl0sImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLnVzLWVhc3QtMS5hbWF6b25hd3MuY29tXC91cy1lYXN0LTFfaXJIV1JsWDdiIiwiY29nbml0bzp1c2VybmFtZSI6Imdvb2dsZV8xMTA5NTc5Mzc4OTIyOTE1ODMzNzkiLCJub25jZSI6IkpUTXdMeU1EUm51LVRZVW1PVlVueEV4cUEtZWt2XzZKdzRvWHRHdFJNZVlWaWROUGE2YkxGU2xtNGJ0eXJQYnVxY0xlZ21CTzZlYXFtemtqRFMtUlFiOHlRTzc2Wi1TSFdTV2x3YTJuTHduMGs5bHRoaVBhT0thNUU1QUJETFFLdUJFeHZhZkZ6QzFLaGwwTzRnbnI0RmdfNlVORFpWd2E4dWU5ajZZMlcxTSIsImF1ZCI6IjZ0ZWk0YnAzbnVwcHA1bXJlbmIwczQ5NmJxIiwiaWRlbnRpdGllcyI6W3sidXNlcklkIjoiMTEwOTU3OTM3ODkyMjkxNTgzMzc5IiwicHJvdmlkZXJOYW1lIjoiR29vZ2xlIiwicHJvdmlkZXJUeXBlIjoiR29vZ2xlIiwiaXNzdWVyIjpudWxsLCJwcmltYXJ5IjoidHJ1ZSIsImRhdGVDcmVhdGVkIjoiMTYxMzIwOTg1OTIwOCJ9XSwidG9rZW5fdXNlIjoiaWQiLCJhdXRoX3RpbWUiOjE2MTUyMDY4M#TksIm5hbWUiOiJWZXJvbmlrYSBEbXl0cnlrIiwiZXhwIjoxNjE1MjEwNDE5LCJpYXQiOjE2MTUyMDY4MTksImVtYWlsIjoidmVyb25pa2EuZG15dHJ5a0BnbWFpbC5jb20ifQ.PvRLka4FHmJUQXy7NrP5siJqdkJ3rEUYMvtSU9QayyIef0GOCMxH_VP359LPl8wmvp1Z5wYRpt6u5nt4ygKELByeiEWLB2y07wFH6MvtHHaIWF2GPpM6QCP5KGZt2gQJgKpmC88D0ZyzasQKYADLIUj7vlX4HOPMnAMNJ3LLnGohHFNVRwJJLeElBp8t4eiQiENOdoFsNd30Y_Rff6PXoyCONY_sg0_-SwG-MdOXIto-VxoMkUGQqrbu3bdkCafckvA_QqiQgFt_xkRoqLOFkAaximA3PTEzA6CsLvq58d4WqctTi-hFZOl3eMlY8oBbeLSc2J0zWNDv_pUO8h_XHg",
+		"property_id": "111" }}, None)
+
+if __name__ == "__main__":
+	main()
